56df120a250a70d4c14c5fecf1c5366c3d1feaaf
NACAMURA Mitsuhiro <m15@m15a.dev>
2025-11-15 22:33:39 +0900

    fix: installation

--- package.nix
+++ package.nix
@@ -30,9 +30,16 @@ stdenv.mkDerivation rec {
   inherit version;
   src = ./.;
   nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ luajit' ];  # for patchShebangs
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
-    install -m755 update.fnl $out/bin/${pname}
-    wrapProgram $out/bin/${pname} --prefix PATH : ${lib.makeBinPath binDeps}
+    cp -r lib $out/
+    install -m755 main.fnl $out/bin/${pname}
+    wrapProgram $out/bin/${pname} \
+        --prefix PATH : ${lib.makeBinPath binDeps} \
+        --set FENNEL_PATH "$out/?.fnl;$out/?/init.fnl" \
+        --set FENNEL_MACRO_PATH "$out/?.fnl;$out/?/init-macros.fnl"
+    runHook postInstall
   '';
 }
